0fe0ded38ac2cff4a61d0ee7cf599da87504ea10
Merhong <133627994+Merhong@users.noreply.github.com>
2023-08-25 12:24:42 +0900

    update repository (#9)

--- src/main/java/shop/mtcoding/boardproject/user/UserRepository.java
+++ src/main/java/shop/mtcoding/boardproject/user/UserRepository.java
@@ -1,7 +1,11 @@
 package shop.mtcoding.boardproject.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    // @Query : executeQuery() 발동
+    @Query(value = "select u from User u where u.username = :username")
+    User findByUsername(@Param("username") String username);
 }
