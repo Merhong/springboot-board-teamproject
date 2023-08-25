474587f045373b6affd78723e68db6c13c34f1c0
k7850 <k79357850@gmail.com>
2023-08-25 11:35:11 +0900

    topic작업

--- src/main/java/shop/mtcoding/boardproject/board/BoardController.java
+++ src/main/java/shop/mtcoding/boardproject/board/BoardController.java
@@ -1,9 +1,35 @@
 package shop.mtcoding.boardproject.board;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable Integer id, HttpServletRequest request) { // Model == HttpServletRequest
+        // Board board = boardService.상세보기(id);
+        // request.setAttribute("board", board);
+        return "bard/detail";
+    }
+
+    @PostMapping("/board/{id}/delete")
+    public @ResponseBody String delete(@PathVariable Integer id){
+        // boardService.삭제하기(id);
+        // return Script.href("/");
+        return "";
+    }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm(@PathVariable Integer id, HttpServletRequest request) {
+        // Board board = boardService.상세보기(id);
+        // request.setAttribute("board", board);
+        return "board/updateForm";
+    }
 
 }
