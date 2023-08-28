5d562ae0d6b66955b6dc6dfd51b38797407110c2
Merhong <zx2938@gmail.com>
2023-08-29 01:06:18 +0900

    헤더, 푸터, 메인페이지, 회원가입페이지, 로그인 페이지 통합 완료

--- src/main/java/shop/mtcoding/boardproject/user/UserController.java
+++ src/main/java/shop/mtcoding/boardproject/user/UserController.java
@@ -2,6 +2,7 @@ package shop.mtcoding.boardproject.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 
@@ -14,4 +15,21 @@ public class UserController {
     @Autowired
     private HttpSession session;
 
+    // Browser URL : IP주소:포트번호/login 입력시 호출
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm"; // view 파일 호출 user/loginForm 파일 호출
+    }
+
+    // Browser URL : IP주소:포트번호/userJoinForm 입력시 호출
+    @GetMapping("/userJoinForm")
+    public String userJoinForm() {
+        return "user/userJoinForm"; // view 파일 호출 user/loginForm 파일 호출
+    }
+
+    // Browser URL : IP주소:포트번호/companyJoinForm 입력시 호출
+    @GetMapping("/companyJoinForm")
+    public String companyJoinForm() {
+        return "user/companyJoinForm"; // view 파일 호출 user/loginForm 파일 호출
+    }
 }
