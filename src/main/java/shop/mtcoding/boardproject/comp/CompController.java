321b9458311681964c8f564c77199c8a39f20599
Merhong <zx2938@gmail.com>
2023-08-29 23:39:16 +0900

    개인 mustache 및 Controller 생성 (17번 개인기업추천 제외)

--- src/main/java/shop/mtcoding/boardproject/comp/CompController.java
+++ src/main/java/shop/mtcoding/boardproject/comp/CompController.java
@@ -2,6 +2,7 @@ package shop.mtcoding.boardproject.comp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CompController {
@@ -12,4 +13,11 @@ public class CompController {
     @Autowired
     private CompService compService;
 
+
+    // Browser URL : IP주소:포트번호/companyJoinForm 입력시 호출
+    // 4_기업회원가입 화면
+    @GetMapping("/compJoinForm")
+    public String compJoinForm() {
+        return "comp/compJoinForm"; // view 파일 호출 comp/compJoinForm 파일 호출
+    }
 }
--- src/main/java/shop/mtcoding/boardproject/user/UserController.java
+++ src/main/java/shop/mtcoding/boardproject/user/UserController.java
@@ -3,6 +3,7 @@ package shop.mtcoding.boardproject.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
@@ -15,26 +16,108 @@ public class UserController {
     @Autowired
     private HttpSession session;
 
-    // Browser URL : IP주소:포트번호/login 입력시 호출
+    // 17_개인기업추천 화면
+    @GetMapping("/userRecommendForm")
+    public String userRecommendForm() {
+        return "user/userRecommendForm";
+    }
+
+    // 16번 북마크 삭제 버튼 POST
+    @PostMapping("/userBookMarkDelete")
+    public String userBookMarkDelete() {
+        return "redirect:/";
+    }
+
+    // 16_개인북마크 화면
+    @GetMapping("/userBookMarkForm")
+    public String userBookMarkForm() {
+        return "user/userBookMarkForm";
+    }
+
+    // 15_개인지원내역 화면
+    @GetMapping("/userApplyList")
+    public String userApplyList() {
+        return "user/userApplyList";
+    }
+
+    // 14번 이력서 수정 버튼 POST
+
+
+
+    // 14번 이력서 삭제 버튼 POST
+
+
+
+
+    // 14_개인이력서관리 화면
+    @GetMapping("/userResumeManage")
+    public String userResumeManage() {
+        return "user/userResumeManage";
+    }
+
+    // 13번 사진수정 버튼 POST
+
+
+
+
+    // 13번 이력서 등록 버튼 POST
+    @PostMapping("/userResumeSave")
+    public String userResumeSave() {
+        return "redirect:/";
+    }
+
+    // 13_개인이력서등록 화면
+    @GetMapping("/userResumeForm")
+    public String userResumeForm() {
+        return "user/userResumeForm";
+    }
+
+    // 12번 수정하기 버튼 POST
+    @PostMapping("/userUpdate")
+    public String userUpdate() {
+        return "redirect:/";
+    }
+
+    // 12_마이페이지 화면
+    @GetMapping("/userMyPage")
+    public String userMyPage() {
+        return "user/userMyPage";
+    }
+
+    // 11번 지원하기 버튼 POST
+    @PostMapping("/userApply")
+    public String userApply() {
+        return "redirect:/";
+    }
+
+    // 11_개인지원하기 화면
+    @GetMapping("/userApplyForm")
+    public String userApplyForm() {
+        return "user/userApplyForm";
+    }
+
+    // 10_개인공고상세보기 화면
+    @GetMapping("/userPostingDetail")
+    public String userPostingDetail() {
+        return "user/userPostingDetail";
+    }
+
+    // 5_로그인 화면
     @GetMapping("/loginForm")
     public String loginForm() {
         return "user/loginForm"; // view 파일 호출 user/loginForm 파일 호출
     }
 
-    // Browser URL : IP주소:포트번호/userJoinForm 입력시 호출
+    // 3_개인회원가입 화면
     @GetMapping("/userJoinForm")
     public String userJoinForm() {
         return "user/userJoinForm"; // view 파일 호출 user/loginForm 파일 호출
     }
 
-    // Browser URL : IP주소:포트번호/companyJoinForm 입력시 호출
-    @GetMapping("/compJoinForm")
-    public String compJoinForm() {
-        return "user/compJoinForm"; // view 파일 호출 user/loginForm 파일 호출
-    }
-
+    // 2_회원가입유형선택 화면
     @GetMapping("/selectJoinForm")
     public String selectJoinForm() {
         return "user/selectJoinForm";
     }
+
 }
