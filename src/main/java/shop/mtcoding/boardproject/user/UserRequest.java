7e34d1c7085bbda316dce9b58f61787c2cb7d8d5
qkrtjdud <qkrtjdudcjst@naver.com>
2023-08-30 17:16:07 +0900

    로그인시 로그인/회원가입 버튼 -> 로그아웃/마이페이지 변경

--- src/main/java/shop/mtcoding/boardproject/user/UserRequest.java
+++ src/main/java/shop/mtcoding/boardproject/user/UserRequest.java
@@ -1,5 +1,14 @@
 package shop.mtcoding.boardproject.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class UserRequest {
 
+    @Getter
+    @Setter
+    public static class LoginDTO {
+        private String email;
+        private String password;
+    }
 }
--- src/main/java/shop/mtcoding/boardproject/user/UserService.java
+++ src/main/java/shop/mtcoding/boardproject/user/UserService.java
@@ -1,8 +1,29 @@
 package shop.mtcoding.boardproject.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import shop.mtcoding.boardproject._core.error.ex.MyException;
+import shop.mtcoding.boardproject.user.UserRequest.LoginDTO;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User 로그인(LoginDTO loginDTO) {
+        User user = userRepository.findByUserEmail(loginDTO.getEmail());
+
+        if (user == null) {
+            throw new MyException("없는 이메일 입니다.");
+        }
+
+        // 패스워드 검증
+        if (!user.getPassword().equals(loginDTO.getPassword())) {
+            throw new MyException("패스워드가 잘못되었습니다!");
+        }
+        return user;
+    }
 
 }
