7b9af1c58cdaa17453323f42fa41de8f10febbba
MMGGHD <tldmseogus@naver.com>
2023-08-30 18:28:33 +0900

    1828

---
+++ src/main/java/shop/mtcoding/boardproject/skill/UserSkillRepository.java
@@ -0,0 +1,6 @@
+package shop.mtcoding.boardproject.skill;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserSkillRepository extends JpaRepository<UserSkill, Integer> {
+}
--- src/test/java/shop/mtcoding/boardproject/BoardProjectApplicationTests.java
+++ src/test/java/shop/mtcoding/boardproject/BoardProjectApplicationTests.java
@@ -1,13 +1,41 @@
 package shop.mtcoding.boardproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import shop.mtcoding.boardproject.posting.PostiongRepository;
+import shop.mtcoding.boardproject.resume.ResumeRepository;
+import shop.mtcoding.boardproject.skill.UserSkillRepository;
 
 @SpringBootTest
 class BoardProjectApplicationTests {
+
+	@Autowired
+	private ResumeRepository resumeRepository;
+
+	@Autowired
+	private UserSkillRepository userSkillRepository;
+
+	@Autowired
+	private PostiongRepository postingRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void selectAllResume() {
+		System.out.println(resumeRepository.findAll());
+	}
+
+	@Test
+	public void selectAllUserSkill() {
+		System.out.println(userSkillRepository.findAll());
+	}
+
+	@Test
+	public void selectAllPosting() {
+		System.out.println(userSkillRepository.findAll());
+	}
 }
